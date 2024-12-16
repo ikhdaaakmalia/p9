54b30f31d79a858c81eb7da682aeaf89aca7723c
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-16 22:34:21 +0700

    membuat fungsi DosenEvent

--- app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/DosenViewModel.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/DosenViewModel.kt
@@ -1,5 +1,13 @@
 package com.ikhdaamel.ucp2.ui.viewmodel
 
+import com.ikhdaamel.ucp2.data.entity.Dosen
+
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jeniKelamin = jeniKelamin
+)
+
 data class DosenEvent(
     val nidn: String ="",
     val nama: String ="",
