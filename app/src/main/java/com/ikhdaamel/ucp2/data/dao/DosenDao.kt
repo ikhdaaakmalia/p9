203b080f9d1a4f72f1f2b1e07665c60fca5620c4
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-16 17:14:27 +0700

    menampilkan semua data dosen

---
+++ app/src/main/java/com/ikhdaamel/ucp2/data/dao/DosenDao.kt
@@ -0,0 +1,12 @@
+package com.ikhdaamel.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.ikhdaamel.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DosenDao {
+    @Query("select * from Dosen")
+    fun getAllDosen() : Flow<List<Dosen>>
+}
