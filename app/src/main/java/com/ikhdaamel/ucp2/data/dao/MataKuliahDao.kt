92ab5d1eb626211ac581677b49a1c6436a1b45c4
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-16 17:28:07 +0700

    membuat fungsi update mata kuliah

--- app/src/main/java/com/ikhdaamel/ucp2/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/data/dao/MataKuliahDao.kt
@@ -2,6 +2,7 @@ package com.ikhdaamel.ucp2.data.dao
 
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.ikhdaamel.ucp2.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
@@ -13,4 +14,7 @@ interface MataKuliahDao {
     suspend fun insertMataKuliah(
         mataKuliah: MataKuliah
     )
+
+    @Update
+    suspend fun updateMataKuliah(mataKuliah: MataKuliah)
 }
