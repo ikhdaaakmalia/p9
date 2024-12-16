13481b1b32143b5b9588536a498ea61c8e56064c
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-16 20:12:05 +0700

    LocalRepoDosen yg diimplementasi dari Dao

---
+++ app/src/main/java/com/ikhdaamel/ucp2/repository/LocalRepoDosen.kt
@@ -0,0 +1,15 @@
+package com.ikhdaamel.ucp2.repository
+
+import com.ikhdaamel.ucp2.data.dao.DosenDao
+import com.ikhdaamel.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepoDosen (private val dosenDao: DosenDao) : RepoDosen{
+    override suspend fun insertDosen(dosen: Dosen) {
+        dosenDao.insertDosen(dosen)
+    }
+
+    override fun getAllDosen() : Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
+    }
+}
