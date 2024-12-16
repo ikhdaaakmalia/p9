8fa8e4fc803765bb053fa80faab9e3cc25b815da
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-16 19:59:28 +0700

    interface repository dosen

---
+++ app/src/main/java/com/ikhdaamel/ucp2/repository/RepoDosen.kt
@@ -0,0 +1,9 @@
+package com.ikhdaamel.ucp2.repository
+
+import com.ikhdaamel.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+interface RepoDosen {
+    suspend fun insertDosen(dosen: Dosen)
+    fun getAllDosen(): Flow<Dosen>
+}
