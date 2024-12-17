314153c3003f26b768541a20e0aafebf767386ab
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-17 09:56:27 +0700

    membuat fungsi MataKuliahEvent

--- app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/MataKuliahViewModel.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/MataKuliahViewModel.kt
@@ -1,5 +1,16 @@
 package com.ikhdaamel.ucp2.ui.viewmodel
 
+import com.ikhdaamel.ucp2.data.entity.MataKuliah
+
+fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
+    kode = kode,
+    nama_mk = nama_mk,
+    sks = sks,
+    semester = semester,
+    jenis = jenis,
+    dosenPengampu = dosenPengampu
+)
+
 data class MataKuliahEvent(
     val kode: String ="",
     val nama_mk: String ="",
