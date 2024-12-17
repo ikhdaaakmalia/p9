bd2fdc31831bbb4f7667060ba991816cf0609e9b
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-17 23:45:49 +0700

    menambahkan @Dao

--- app/src/main/java/com/ikhdaamel/ucp2/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/data/dao/MataKuliahDao.kt
@@ -1,5 +1,6 @@
 package com.ikhdaamel.ucp2.data.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
@@ -7,6 +8,7 @@ import androidx.room.Update
 import com.ikhdaamel.ucp2.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface MataKuliahDao {
     @Query("Select * from MataKuliah order by nama_mk ASC")
     fun getAllMataKuliah(): Flow<List<MataKuliah>>
