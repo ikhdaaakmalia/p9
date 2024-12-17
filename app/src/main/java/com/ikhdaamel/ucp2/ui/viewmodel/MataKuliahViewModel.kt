a38b787ad4f237fba44fc502b492bd82d64be4df
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-17 10:14:28 +0700

    membuat class FormErr  untuk memastikan tdk ada data yg kosong

--- app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/MataKuliahViewModel.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/MataKuliahViewModel.kt
@@ -2,6 +2,19 @@ package com.ikhdaamel.ucp2.ui.viewmodel
 
 import com.ikhdaamel.ucp2.data.entity.MataKuliah
 
+data class FormErrorState(
+    val kode: String? = null,
+    val nama_mk: String? = null,
+    val sks: String? = null,
+    val semester: String? = null,
+    val jenis: String? = null,
+    val dosenPengampu: String? = null
+){
+    fun isValid(): Boolean{
+        return kode == null && nama_mk == null && sks == null && semester == null && jenis == null && dosenPengampu == null
+    }
+}
+
 fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
     kode = kode,
     nama_mk = nama_mk,
