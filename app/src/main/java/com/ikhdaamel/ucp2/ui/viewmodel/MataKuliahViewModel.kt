7815741fa2d855ba39b37da65cf7d0520097e801
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-17 10:36:12 +0700

    membuat fungsi updateState & privat fungsi validateFields

--- app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/MataKuliahViewModel.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/MataKuliahViewModel.kt
@@ -1,6 +1,33 @@
 package com.ikhdaamel.ucp2.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
 import com.ikhdaamel.ucp2.data.entity.MataKuliah
+import com.ikhdaamel.ucp2.repository.RepoMataKuliah
+
+class MataKuliahViewModel(private val repoMataKuliah: RepoMataKuliah): ViewModel(){
+    var uiState by mutableStateOf(MatKulUiState())
+
+    fun updateState (mataKuliahEvent: MataKuliahEvent) {
+        uiState = uiState.copy(
+            mataKuliahEvent = mataKuliahEvent
+        )
+    }
+
+    private fun validateFields(): Boolean{
+        val event = uiState.mataKuliahEvent
+        val errorState = formErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "Kode harus diisi",
+            nama_mk = if (event.nama_mk.isNotEmpty()) null else "Nama Mata Kuliah harus diisi",
+            sks = if (event.sks.isNotEmpty()) null else "SKS harus diisi",
+            semester = if (event.semester.isNotEmpty()) null else "Semester harus diisi",
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis Mata Kuliah harus diisi",
+            dosenPengampu = if (event.dosenPengampu.isNotEmpty()) null else "Dosen Pengampu harus diisi",
+        )
+        uiState = uiState(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
 
 data class MatKulUiState(
     val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
