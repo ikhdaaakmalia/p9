fc895c580454566448d2ee7e5ff58cd81573a015
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-17 09:54:14 +0700

    membuat class MataKuliahEvent

---
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/MataKuliahViewModel.kt
@@ -0,0 +1,10 @@
+package com.ikhdaamel.ucp2.ui.viewmodel
+
+data class MataKuliahEvent(
+    val kode: String ="",
+    val nama_mk: String ="",
+    val sks: String ="",
+    val semester: String ="",
+    val jenis: String ="",
+    val dosenPengampu: String =""
+)
