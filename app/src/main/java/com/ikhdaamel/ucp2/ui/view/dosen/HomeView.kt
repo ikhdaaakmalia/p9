f128c1a876afce76379cd0ff635907de7ead2f14
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-18 17:14:11 +0700

    membuat halaman home yg terdapat 2 button & text di dalamnya

---
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/view/dosen/HomeView.kt
@@ -0,0 +1,85 @@
+package com.ikhdaamel.ucp2.ui.view.dosen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+
+fun HomeView(){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFD1DC))
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Selamat Datang",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        )
+        Text(
+            text = "Pilih Sesuai Data yang Akan Diisi"
+        )
+        Spacer(modifier = Modifier.padding(15.dp))
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+            Button(
+                onClick = {  },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 10.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = "Dosen Icon",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp)) // Space between icon and text
+                Text(text = "Dosen")
+            }
+            Button(
+                onClick = {  },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 10.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = "Mata Kuliah Icon",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp)) // Space between icon and text
+                Text(text = "Mata Kuliah")
+            }
+        }
+
+    }
+}
