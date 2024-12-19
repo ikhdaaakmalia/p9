3a1ac597631b9e040b45c7a6edd15f1b9c67f52c
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-19 22:15:43 +0700

    membuat fungsi BodyHomMatKulView dan fungsi HomeMatKulView

--- app/src/main/java/com/ikhdaamel/ucp2/ui/view/matakuliah/HomeMatKulView.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/view/matakuliah/HomeMatKulView.kt
@@ -1,30 +1,141 @@
 package com.ikhdaamel.ucp2.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ikhdaamel.ucp2.data.entity.MataKuliah
+import com.ikhdaamel.ucp2.ui.customwidget.TopAppBar
+import com.ikhdaamel.ucp2.ui.viewmodel.matakuliah.HomeMatKulUiState
+import com.ikhdaamel.ucp2.ui.viewmodel.matakuliah.HomeMatKulViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeMatKulView(
+    viewModel: HomeMatKulViewModel = viewModel (),
+    onAddMatKul: () -> Unit = {},
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Mata Kuliah",
+                showBackButton = false,
+                onBack = {}
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMatKul,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mata Kuliah"
+                )
+            }
+        }
+    ){
+        innerPadding ->
+        val homeMatKulUiState by viewModel.homeMatKulUiState.collectAsState()
+
+        BodyHomMatKulView(
+            homeMatKulUiState = homeMatKulUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun BodyHomMatKulView(
+    homeMatKulUiState: HomeMatKulUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeMatKulUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeMatKulUiState.isError -> {
+            LaunchedEffect(homeMatKulUiState.errorMessage) {
+                homeMatKulUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        homeMatKulUiState.listMatKul.isEmpty() -> {
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak Ada Data Mata Kuliah",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            ListMataKuliah(
+                listMatKul = homeMatKulUiState.listMatKul,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 @Composable
 fun ListMataKuliah(
@@ -39,7 +150,7 @@ fun ListMataKuliah(
             itemContent = {matkul ->
                 CardMataKuliah(
                     matkul = matkul,
-                    onClick = {onClick(matkul.kode_mk)}
+                    onClick = {onClick(matkul.kode)}
                 )
             }
         )
