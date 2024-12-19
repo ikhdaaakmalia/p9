b1c408effb86d8441ff63ee2bcb388e01bba1ba9
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-19 14:04:45 +0700

    menambhakan initializer HomeViewModel dan HomeDosenViewModel

--- app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -1,16 +1,30 @@
 package com.ikhdaamel.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.ikhdaamel.ucp2.KRSApp
 import com.ikhdaamel.ucp2.ui.viewmodel.dosen.DosenViewModel
+import com.ikhdaamel.ucp2.ui.viewmodel.dosen.HomeDosenViewModel
 import com.ikhdaamel.ucp2.ui.viewmodel.matakuliah.DetailMatKulViewModel
 import com.ikhdaamel.ucp2.ui.viewmodel.matakuliah.MataKuliahViewModel
 import com.ikhdaamel.ucp2.ui.viewmodel.matakuliah.UpdateMatKulViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                KRSApp().containerApp.repoDosen,
+                KRSApp().containerApp.repoMataKuliah
+            )
+        }
+        initializer {
+            HomeDosenViewModel(
+                KRSApp().containerApp.repoDosen
+            )
+        }
         initializer {
             DosenViewModel(
                 KRSApp().containerApp.repoDosen
@@ -31,7 +45,10 @@ object PenyediaViewModel{
           DetailMatKulViewModel(
                 createSavedStateHandle(),
                 KRSApp().containerApp.repoMataKuliah
-            )
+          )
         }
     }
 }
+
+fun CreationExtras.krsApp(): KRSApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KRSApp)
