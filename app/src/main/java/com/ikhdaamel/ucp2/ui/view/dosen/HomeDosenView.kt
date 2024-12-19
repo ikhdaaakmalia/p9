d603c995e4d7fd7d5484929489d579c512f32d6c
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-19 21:18:40 +0700

    membuat fungsi dosen view

--- app/src/main/java/com/ikhdaamel/ucp2/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/view/dosen/HomeDosenView.kt
@@ -1,28 +1,140 @@
 package com.ikhdaamel.ucp2.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ikhdaamel.ucp2.data.entity.Dosen
+import com.ikhdaamel.ucp2.ui.customwidget.TopAppBar
+import com.ikhdaamel.ucp2.ui.viewmodel.dosen.HomeDosenUiState
+import com.ikhdaamel.ucp2.ui.viewmodel.dosen.HomeDosenViewModel
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+@Composable
+fun HomeDosenView(
+    viewModel: HomeDosenViewModel = viewModel(),
+    onAddDsn: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = false,
+                onBack = {}
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDsn,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen"
+                )
+            }
+        }
+    ){
+        innerPadding ->
+        val homeDosenUiState by viewModel.homeDosenUiState.collectAsState()
+
+        BodyHomeDosenView(
+            homeDosenUiState = homeDosenUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun BodyHomeDosenView(
+    homeDosenUiState: HomeDosenUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val SnackbarHostState = remember {SnackbarHostState()}
+    when{
+        homeDosenUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeDosenUiState.isError -> {
+            LaunchedEffect(homeDosenUiState.errorMessage) {
+                homeDosenUiState.errorMessage?.let{ message ->
+                    coroutineScope.launch {
+                        SnackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        homeDosenUiState.listDosen.isEmpty() -> {
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak Ada Data Mahasiswa",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            ListDosen(
+                listDosen = homeDosenUiState.listDosen,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 @Composable
 fun ListDosen(
