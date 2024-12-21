c529f976c144d613a9822cc3c8cca081f337aabc
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-21 07:53:12 +0700

    koreksi

--- app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/matakuliah/UpdateMatKulViewModel.kt
+++ app/src/main/java/com/ikhdaamel/ucp2/ui/viewmodel/matakuliah/UpdateMatKulViewModel.kt
@@ -9,20 +9,24 @@ import androidx.lifecycle.viewModelScope
 import com.ikhdaamel.ucp2.data.entity.MataKuliah
 import com.ikhdaamel.ucp2.repository.RepoMataKuliah
 import com.ikhdaamel.ucp2.ui.navigation.DestinasiUpdate
+import com.ikhdaamel.ucp2.ui.viewmodel.HomeMataKulUiState
+import com.ikhdaamel.ucp2.ui.viewmodel.MatKulFormErrorState
+import com.ikhdaamel.ucp2.ui.viewmodel.MataKuliahEvent
+import com.ikhdaamel.ucp2.ui.viewmodel.toMataKuliahEntity
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
-data class MatKulUiState(
-    val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
-    val isEntryValid: MatKulFormErrorState = MatKulFormErrorState(),
-    val SnackBarMessage: String? = null
-)
+//data class MatKulUiState(
+//    val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+//    val isEntryValid: MatKulFormErrorState = MatKulFormErrorState(),
+//    val SnackBarMessage: String? = null
+//)
 class UpdateMatKulViewModel (
     savedStateHandle: SavedStateHandle,
     private val repoMataKuliah: RepoMataKuliah
 ): ViewModel() {
-    var updateUiState by mutableStateOf(MatKulUiState())
+    var updateUiState by mutableStateOf(HomeMataKulUiState())
         private set
     private val _kode: String = checkNotNull(savedStateHandle[DestinasiUpdate.KODE])
 
@@ -63,28 +67,28 @@ class UpdateMatKulViewModel (
                 try {
                     repoMataKuliah.updateMataKuliah(currentEvent.toMataKuliahEntity())
                     updateUiState = updateUiState.copy(
-                        SnackBarMessage = "Data Berhasil Diupdate",
+                        snackBarMessage = "Data Berhasil Diupdate",
                         mataKuliahEvent = MataKuliahEvent(),
                         isEntryValid = MatKulFormErrorState()
                     )
-                    println("snackBarMessage diatur: ${updateUiState.SnackBarMessage}")
+                    println("snackBarMessage diatur: ${updateUiState.snackBarMessage}")
                 } catch (e: Exception) {
                     updateUiState = updateUiState.copy(
-                        SnackBarMessage = "Data Gagal Diupdate"
+                        snackBarMessage = "Data Gagal Diupdate"
                     )
                 }
             }
         } else {
             updateUiState = updateUiState.copy(
-                SnackBarMessage = "Data Gagal diupdate"
+                snackBarMessage = "Data Gagal diupdate"
             )
         }
     }
     fun resetSnackBarMessage(){
-        updateUiState = updateUiState.copy(SnackBarMessage = null)
+        updateUiState = updateUiState.copy(snackBarMessage = null)
     }
 }
 
-fun MataKuliah.toUiStateMatKul() : MatKulUiState = MatKulUiState(
+fun MataKuliah.toUiStateMatKul() : HomeMataKulUiState = HomeMataKulUiState(
     mataKuliahEvent = this.toDetailUiEvent()
 )
